8e5adcb51d7921a4fb49ef2d47d10de0d3c2335e
國益良太 <kunimasuryota@kunimasumasuryoutanoMacBook-Pro.local>
2018-10-12 13:34:15 +0900

    add new file

--- gitsample2/unixensyu/uensyu12_2.c
+++ gitsample2/unixensyu/uensyu12_2.c
@@ -1,6 +1,5 @@
-/* frksmpl1.c - fork(2)で子プロセスを作るプログラム */
-#include <stdio.h>
-#include <unistd.h>
+#include <stdio.h>  //scanf,printf
+#include <unistd.h> //sleep,fork
 int main(void) {
   pid_t pid; /* fork()からの返り値 */
   int i;
@@ -8,8 +7,7 @@ int main(void) {
      このプログラムの新しいプロセスがコピーされ作られる. */
   pid = fork();
   /* 返り値によって，自分が親か子かを判断する */
-  if(pid == -1)
-    /* forkに失敗 */
+  if(pid == -1)  /* forkに失敗 */
     fprintf(stderr, "fork has failed.\n");
   else if(pid == 0){  /* 子プロセス */
     printf("I am a child process ID is:%d\n",getpid());  //子自身のプロセスIDを取得
