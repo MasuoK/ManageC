39bf6c50fbac187f599e59e34977670a5e7f51a2
MasuoK <mas3920gml@icloud.com>
2019-05-28 00:52:44 +0900

    add new file

---
+++ Vall/Input1/Input1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int a;
+
+	cout << "‚È‚É‚©“ü‚ê‚Ä‚Ë > ";
+	cin >> a;
+
+	cout << "‚»‚Ì”‚ð3‚ÅŠ„‚Á‚½—]‚è‚Í"
+		<< a % 3 << "‚Å‚·B" << endl;
+
+	return 0;
+}
